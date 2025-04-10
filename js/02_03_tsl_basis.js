d8602867952fd8c8cc9ef1454dea5d2318ec9fdf
webdoli <topofweird@gmail.com>
2025-04-10 16:33:13 +0900

    tsl study ver01

---
+++ js/02_03_tsl_basis.js
@@ -0,0 +1,78 @@
+import * as THREE from 'three/webgpu'
+import { abs, If, Fn, vec2, color, texture, convertColorSpace, positionLocal, rotateUV, time } from 'three/tsl'
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js'
+
+const scene = new THREE.Scene()
+const camera = new THREE.PerspectiveCamera(
+  75,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  10
+)
+camera.position.z = 1
+
+const renderer = new THREE.WebGPURenderer(); // webGLRenderer가 아닌 WebGPURenderer사용함
+
+renderer.setSize(window.innerWidth, window.innerHeight)
+document.body.appendChild(renderer.domElement)
+renderer.setAnimationLoop(animate); // 애니메이션 루프가 효율적으로 변함
+
+window.addEventListener('resize', function () {
+  camera.aspect = window.innerWidth / window.innerHeight
+  camera.updateProjectionMatrix()
+  renderer.setSize(window.innerWidth, window.innerHeight)
+})
+
+const controls = new OrbitControls(camera, renderer.domElement)
+controls.enableDamping = true
+
+const main = Fn(() => {
+    const p = positionLocal.toVar();
+    p.assign( rotateUV( p.xyz, time, vec2() ))
+    // If( p.x.lessThan(-.45), () => {
+    //     p.z = 1;
+    // })
+
+    // If( p.x.greaterThan(.45), () => {
+    //     p.z = 1;
+    // })
+
+    // If( abs(p.x).greaterThan(.45), () => {
+    //     p.z = 1;
+    // })
+
+    // If( abs(p.y).greaterThan(.45), () => {
+    //     p.z = 1;
+    // })
+
+    return p
+});
+
+const material = new THREE.NodeMaterial(); // 퐁, 베이직과 같은 재질이 아님 
+// material.fragmentNode = color('crimson');
+// material.fragmentNode = texture(
+//     new THREE.TextureLoader().load('https://sbcode.net/img/grid.png')
+// )
+
+// material.fragmentNode = convertColorSpace(
+//     texture(new THREE.TextureLoader().load('https://sbcode.net/img/grid.png')),
+//     THREE.SRGBColorSpace,
+//     THREE.LinearSRGBColorSpace
+// );
+
+material.fragmentNode = main();
+
+
+const mesh = new THREE.Mesh(new THREE.BoxGeometry(), material)
+scene.add(mesh);
+
+renderer.debug.getShaderAsync(scene, camera, mesh).then((e) => {
+    console.log(e.vertexShader)
+    // console.log(e.fragmentShader)
+  })
+
+function animate() {
+  controls.update()
+
+  renderer.render(scene, camera)
+}
